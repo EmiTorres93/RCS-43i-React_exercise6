2a44853f99dd3b94406e912e876c47f5aad45fb4
EmiTorres93 <emilianat1992@gmail.com>
2023-07-22 12:21:24 -0300

    Terminando detalles inicialiazación proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="mainPage"></Container>
+      <footer className="footer text-center pt-3">
+        <p>&copy, Exercise_6 Todos los derechos reservados</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
